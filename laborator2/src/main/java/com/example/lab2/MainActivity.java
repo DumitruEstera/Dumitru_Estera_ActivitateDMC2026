6e51b39b3b782a5db9b3660956116e3bc6edade4
Estera Dumitru <dumitruestera13@gmail.com>
2026-02-24 09:15:51 +0200

    first commit - lab2

---
+++ laborator2/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button myButton = findViewById(R.id.button);
+        TextView myTextView = findViewById(R.id.textViewCentral);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                myTextView.setText(R.string.mesaj_dupa_apasare_buton);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
